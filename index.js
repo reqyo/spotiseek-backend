3854f95af856a3f8b4bf1887a07241a4b0309adc
Zakariah Drummond <zak@mintyfreshdev.co.uk>
2022-07-26 22:11:31 +0100

    callback hell im having an argument

--- index.js
+++ index.js
@@ -2,46 +2,56 @@ const slsk = require('slsk-client')
 const music = require('./music.json')
 require('dotenv').config()
 
-let downloaded = true
+let client
 
 slsk.connect(
   {
     user: process.env.USER,
     pass: process.env.PASS
   },
-  (err, client) => {
-    console.log(err)
-    if (client) {
-      console.log(client)
-      // console.log(err)
-      for (
-        let index = 0;
-        index < music.length;
-        index = downloaded ? index + 1 : index
-      ) {
-        downloaded = false
-        console.log('About to search')
-        client.search(
+  (err, c) => {
+    if (err) {
+      throw new Error(err)
+    }
+    if (c) console.log("I'm in.")
+    client = c
+    searchForMusic(music)
+  }
+)
+let downloaded = true
+
+function searchForMusic(m) {
+  const length = m.length
+  console.log(length)
+  let currentIndex = 4
+
+  do {
+    downloaded = false
+    console.log(`Current Song Number ${currentIndex}`)
+    console.log('About to search')
+    client.search(
+      {
+        req: m[currentIndex].track.name,
+        timeout: 5000
+      },
+      (err, res) => {
+        if (err) return console.log(err)
+        const files = res.filter((item) => item.slots && item.bitrate === 320)
+        if (files.length === 0) return
+        console.log(files)
+        console.log(files.length, `Got the search: ${files[0]}`)
+        console.log('About to download')
+        client.download(
           {
-            req: music[index].track.name,
-            timeout: 5000
+            file: files[0]
           },
-          (err, res) => {
-            if (err) return console.log(err)
-            client.download(
-              {
-                file: res[0],
-                path: `${__dirname}/${track.track.name}.mp3`
-              },
-              (err, data) => {
-                if (data) {
-                  downloaded = true
-                }
-              }
-            )
+          (err, data) => {
+            console.log('Downloaded!')
+            currentIndex++
+            downloaded = true
           }
         )
       }
-    }
-  }
-)
+    )
+  } while (currentIndex < length && downloaded)
+}
